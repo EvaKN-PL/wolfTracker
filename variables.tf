4f43d558d2d232893beff0616ca698c5609789c1
Ewa Nizioł <kiepas.ewa@gmail.com>
2026-02-05 14:20:03 +0100

    change

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "region" {
     description = "Region in AWS"
     type        = string
-    default     = "eu-central-1"
+    
 }
 
 variable "env" {
     description = "Type of environment"
     type        = string
-    default     = "dev"
+   
   
 }
