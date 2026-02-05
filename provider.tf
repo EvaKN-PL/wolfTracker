f566b1aa144e3e079c22f14884028e227a665e2a
Ewa Nizioł <kiepas.ewa@gmail.com>
2026-02-05 14:44:19 +0100

    for apply

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "wolftracker2026" 
+    key            = "wolf-tracker/terraform.tfstate"      
+    region         = "eu-central-1"
+    encrypt        = true                                    
+}
+}
+
 provider "aws" {
   region = var.region
 }
