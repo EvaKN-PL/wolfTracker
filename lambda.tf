0edad55f319bc0557fc98e76a0aba35dbd23fc71
Ewa Nizioł <kiepas.ewa@gmail.com>
2026-02-05 15:49:52 +0100

    zmiany w lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -58,6 +58,11 @@ resource "aws_iam_policy" "lambda_dynamodb" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.main.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
 resource "aws_iam_role_policy_attachment" "lambda_attach" {
   role       = aws_iam_role.main.name
   policy_arn = aws_iam_policy.lambda_dynamodb.arn
