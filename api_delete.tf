72c8a29d62823e2f5309cc5886e4300ac7057d4a
Ewa Nizioł <kiepas.ewa@gmail.com>
2026-02-16 16:57:33 +0100

    change in api

--- api_delete.tf
+++ api_delete.tf
@@ -2,6 +2,8 @@ resource "aws_apigatewayv2_integration" "delete_reports" {
     api_id = aws_apigatewayv2_api.main.id
     integration_type = "AWS_PROXY"
     integration_uri = aws_lambda_function.delete_reports.invoke_arn
+
+    payload_format_version = "2.0"
   
 }
 
--- api_get.tf
+++ api_get.tf
@@ -2,6 +2,8 @@ resource "aws_apigatewayv2_integration" "get_reports" {
     api_id = aws_apigatewayv2_api.main.id
     integration_type = "AWS_PROXY"
     integration_uri = aws_lambda_function.get_reports.invoke_arn
+
+    payload_format_version = "2.0"
   
 }
 
