00a86209533966b9034bdf6101ea14972f20f08a
Ewa Nizioł <kiepas.ewa@gmail.com>
2026-02-16 17:45:35 +0100

    change in storage

--- storage.tf
+++ storage.tf
@@ -2,3 +2,14 @@ resource "aws_s3_bucket" "photo" {
     bucket = "${var.env}-tracks-photos"
   
 }
+
+resource "aws_s3_bucket_cors_configuration" "photo_cors" {
+  bucket = aws_s3_bucket.photo.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"] 
+    allowed_origins = ["*"]           
+    expose_headers  = ["ETag"]
+  }
+}
